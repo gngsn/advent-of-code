f3e3838f7d94f994f525c7bb7c74e6160d5fba9a
gngsn <rudtjs4540@gmail.com>
2022-12-04 20:51:08 +0900

    2022 - day4 👊🏻

--- 2022/src/main/kotlin/day4/part2.kt
+++ 2022/src/main/kotlin/day4/part2.kt
@@ -5,6 +5,7 @@ import com.gngsn.year2022.readFile
 fun main(args: Array<String>) {
     readFile("src/main/kotlin/day4/input.txt")
         .map { line -> line.split(",", "-").map { c -> c.toInt() } }
-        .filter { r -> (r[0] >= r[2] && r[1] <= r[3]) || (r[0] <= r[2] && r[1] >= r[3]) }
-        .count().subscribe {x -> println("$x")}
+        .filter { r -> !(r[1] < r[2] || r[0] > r[3]) }
+        .count()
+        .subscribe {x -> println("$x")}
 }
