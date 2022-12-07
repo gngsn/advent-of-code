f15d12324464c7a9c715f3cf10fb1f540dad5825
gngsn <rudtjs4540@gmail.com>
2022-12-08 02:11:57 +0900

    2022 - day7 👊🏻: part1

--- 2022/src/main/kotlin/day7/part1.kt
+++ 2022/src/main/kotlin/day7/part1.kt
@@ -3,76 +3,117 @@ package com.gngsn.year2022.day7
 import com.gngsn.year2022.readFile
 
 fun main(args: Array<String>) {
-    var root: File? = null
-    var pwd: File? = null
+    var root: Directory? = null
+    var pwd: Directory? = null
 
     readFile("src/main/kotlin/day7/input.txt")
         .doOnNext { raw ->
             val line = raw.split(" ")
 
             if (pwd == null) {
-                root = File(line[2], null)
+                root = Directory(line[2], null)
                 pwd = root
                 return@doOnNext
             }
 
-            println("line: $line | curFile: $pwd")
-
             if (line[0].isCommand()) {
                 if ("cd" == line[1]) {
 
-                    if (line[2] == "..") {
-                        pwd = pwd?.parent
+                    pwd = if (line[2] == "..") {
+                        pwd?.parent
                     } else {
-                        println("CD CMD - curFile : $pwd")
-                        pwd = pwd?.children?.first { c -> c.name == line[2] }
+                        pwd?.children?.first { c -> c.name == line[2] } as Directory?
                     }
                 }
 
                 return@doOnNext
             }
 
-            val child = if (line[0] == "dir") File(line[1], pwd) else File(line[1], pwd, line[0].toLong())
+            val child = if (line[0] == "dir") Directory(line[1], pwd) else File(line[1], pwd, line[0].toLong())
             pwd?.addChild(child)
 
-        }.subscribe  { line ->
-            println("\n[line: $line]\n${root?.print()}\n\n")
-        }
+        }.subscribe()
+
+    println(root?.print())
+    println("TOTAL: ${root?.sumAtMost()}")
 }
+
+/* OUTPUT
+
+Directory(name=/, size=48381165)
+  - Directory(name=a, size=94853)
+	  - Directory(name=e, size=584)
+		  - File(name=i, size=584)
+	  - File(name=f, size=29116)
+	  - File(name=g, size=2557)
+	  - File(name=h.lst, size=62596)
+  - File(name=b.txt, size=14848514)
+  - File(name=c.dat, size=8504156)
+  - Directory(name=d, size=24933642)
+	  - File(name=j, size=4060174)
+	  - File(name=d.log, size=8033020)
+	  - File(name=d.ext, size=5626152)
+	  - File(name=k, size=7214296)
+
+TOTAL: 95437
+
+ */
 
 private fun String.isCommand() = this == "$"
 
-data class File(
-    val name: String,
-    val parent: File?,
-    var size: Long = 0,
-    var children: MutableList<File>? = null
-) {
+class Directory(
+    override val name: String,
+    parent: Directory?,
+    var children: MutableList<File> = mutableListOf()
+) : File(name, parent) {
+
     fun addChild(file: File) {
-        if (this.children.isNullOrEmpty()) this.children = mutableListOf()
-        this.children!!.add(file)
+        this.children.add(file)
     }
 
-    fun getFileSize(): Long {
-        if (children.isNullOrEmpty()) return this.size
+
+    fun sumAtMost(atMost: Int = 100000): Long {
+        return children.filterIsInstance<Directory>().map { ch ->
+            val sz = ch.getFileSize()
+            ch.sumAtMost() + if (sz < atMost) sz else 0
+        }.sum()
+    }
+
+    override fun print(dept: Int): String {
+        var structure = "$this\n"
+        children.forEach { ch -> structure += "\t".repeat(dept) + "  - " + ch.print(dept + 1) }
+
+        return structure
+    }
+
+
+    override fun getFileSize(): Long {
         var dirSize: Long = 0
-        children!!.forEach() { c -> dirSize += c.size }
+        this.children.forEach { c -> dirSize += c.getFileSize() }
 
         return dirSize
+    }
+
+    override fun toString(): String {
+        return "Directory(name=$name, size=${this.getFileSize()})"
+    }
+}
+
+open class File(
+    open val name: String,
+    val parent: Directory?,
+    var size: Long = 0
+) {
+
+    open fun print(dept: Int = 0): String {
+        return "$this\n"
     }
 
     override fun toString(): String {
         return "File(name=$name, size=${this.getFileSize()})"
     }
 
-    fun print(dept: Int = 0): String {
-        var structure = "File(name=$name, size=${this.getFileSize()})\n"
-
-        if (!children.isNullOrEmpty()) {
-            children?.forEach() { ch -> structure += "\t".repeat(dept) + "  - " + ch.print(dept+1) }
-        }
-
-        return structure
-    }
+    open fun getFileSize(): Long = this.size
 }
+
 
