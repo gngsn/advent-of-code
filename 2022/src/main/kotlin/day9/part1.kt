a8ab5c79ecd924784776831d44c76c7c6642e6c2
gngsn <rudtjs4540@gmail.com>
2022-12-10 19:12:32 +0900

    2022 - day9 👊🏻: part1

---
+++ 2022/src/main/kotlin/day9/part1.kt
@@ -0,0 +1,54 @@
+package com.gngsn.year2022.day9
+
+import com.gngsn.year2022.readFile
+import kotlin.math.pow
+import kotlin.math.sqrt
+
+
+fun main() {
+    val trace = mutableSetOf<Pair<Int, Int>>()
+    val start: Pair<Int, Int> = Pair(0, 0)
+    var head = start
+    var tail = start
+
+    readFile("src/main/kotlin/day9/input.txt")
+        .doOnNext { line ->
+
+            val cmd = line.split(" ")
+            val dir = cmd[0]
+            var mv = cmd[1].toInt()
+
+            while(mv-- > 0) {
+
+                head = move(head, dir)
+                val distance = sqrt((head.first - tail.first).toDouble().pow(2) +
+                        (head.second - tail.second).toDouble().pow(2))
+
+                if (distance > 2) {
+                    tail = when (dir) {
+                        "U" -> Pair(head.first, head.second-1)
+                        "D" -> Pair(head.first, head.second+1)
+                        "R" -> Pair(head.first-1, head.second)
+                        "L" -> Pair(head.first+1, head.second)
+                        else -> throw IllegalArgumentException("Not supported command")
+                    }
+                } else if (distance == 2.0) {
+                    tail = move(tail, dir)
+                }
+
+                trace.add(tail)
+            }
+        }.subscribe()
+
+    println("VISIT: ${trace.size}")
+}
+
+fun move(pos: Pair<Int, Int>, dir: String): Pair<Int, Int> {
+    return when (dir) {
+        "U" -> Pair(pos.first, pos.second + 1)
+        "D" -> Pair(pos.first, pos.second - 1)
+        "R" -> Pair(pos.first + 1, pos.second)
+        "L" -> Pair(pos.first - 1, pos.second)
+        else -> throw IllegalArgumentException("Not supported command")
+    }
+}
