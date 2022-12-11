892c0dc9a56adbb087315350cca2f7b7e30ce990
gngsn <rudtjs4540@gmail.com>
2022-12-11 20:39:35 +0900

    2022 - day9 👊🏻: part1

--- 2022/src/main/kotlin/day9/part1.kt
+++ 2022/src/main/kotlin/day9/part1.kt
@@ -1,6 +1,7 @@
 package com.gngsn.year2022.day9
 
 import com.gngsn.year2022.readFile
+import kotlin.math.abs
 import kotlin.math.pow
 import kotlin.math.sqrt
 
@@ -36,19 +37,33 @@ fun main() {
                     tail = move(tail, dir)
                 }
 
+                println("{head=$head, tail =$tail}")
                 trace.add(tail)
             }
+            println("{trace=$trace}")
         }.subscribe()
 
     println("VISIT: ${trace.size}")
 }
 
-fun move(pos: Pair<Int, Int>, dir: String): Pair<Int, Int> {
-    return when (dir) {
+private fun move(pos: Pair<Int, Int>, dir: String): Pair<Int, Int> =
+     when (dir) {
         "U" -> Pair(pos.first, pos.second + 1)
         "D" -> Pair(pos.first, pos.second - 1)
         "R" -> Pair(pos.first + 1, pos.second)
         "L" -> Pair(pos.first - 1, pos.second)
         else -> throw IllegalArgumentException("Not supported command")
     }
+
+
+fun move2(from: Pair<Int, Int>, to: Pair<Int, Int>): MutableSet<Pair<Int, Int>> {
+    val track = mutableSetOf<Pair<Int, Int>>()
+    println("move from{x=${from.first}, y=${from.second}}, to{x=${to.first}, y=${to.second}}")
+    val xweight = if (to.first < 0) -1 else 1
+    val yweight = if (to.second < 0) -1 else 1
+
+    (0 until abs(from.first - to.first)).forEach { i -> track.add(Pair(from.first + (i * xweight) + 1, from.second)) }
+    (0 until abs(from.second - to.second)).forEach { i -> track.add(Pair(from.first, from.second + (i * yweight) + 1)) }
+
+    return track
 }
