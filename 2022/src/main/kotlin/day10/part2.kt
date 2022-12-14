1dbef704abd16730dfd5a2682d5858c0e36b8185
gngsn <rudtjs4540@gmail.com>
2022-12-14 21:38:35 +0900

    2022 - day10: refac part2

--- 2022/src/main/kotlin/day10/part2.kt
+++ 2022/src/main/kotlin/day10/part2.kt
@@ -11,27 +11,21 @@ fun main() {
 
     readFile("src/main/kotlin/day10/input.txt")
         .doOnNext { raw ->
+            handleSign(graph, cycle, sign)
+
             when(raw.substringBefore(" ")) {
                 Instr.ADDX.code -> {
-                    cycle += 1
-                    handleSign(graph, cycle - 1, sign)
-                    cycle += 1
-                    handleSign(graph, cycle - 1, sign)
+                    cycle += 2
+                    handleSign(graph, cycle-1, sign)
                     sign += raw.substringAfter(" ").toInt()
                 }
-                Instr.NOOP.code -> {
-                    cycle += 1
-                    handleSign(graph, cycle - 1, sign)
-                }
-                else -> throw IllegalArgumentException("not supported instruction.")
+                Instr.NOOP.code -> cycle += 1
+                else -> throw IllegalArgumentException("$raw not supported instruction.")
             }
         }.takeUntil{ cycle >= 240 }
         .subscribe()
 
-    graph.forEach { g ->
-        g.forEach(System.out::print)
-        println("")
-    }
+    println(graph.joinToString("\n") { it.joinToString("") })
 }
 
 fun handleSign(graph: List<MutableList<Char>>, cycle: Int, sign: Int) {
